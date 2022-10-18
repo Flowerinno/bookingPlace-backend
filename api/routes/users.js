27a880919e9c666d5353c3b6173161fecff0f37d
Kononov <akellastoopi@gmail.com>
2022-10-18 11:59:34 +0300

    decreased api response time via package mongodb

--- api/routes/users.js
+++ api/routes/users.js
@@ -7,22 +7,9 @@ import {
 	getAllUsers,
 } from "../contollers/user.js";
 
-import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
+import { verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
-//cheks whether the user has auth token or not
-// router.get("/checkauthentication", verifyToken, (req, res) => {
-// 	res.send("Hello user , you are logged in");
-// });
-
-// router.get("/checkuser/:id", verifyUser, (req, res) => {
-// 	res.send("Hello user ,you can delete your account");
-// });
-
-// router.get("/checkadmin/:id", verifyAdmin, (req, res) => {
-// 	res.send("You are logged in and you are an admin!");
-// });
-
 
 //CREATE
 router.post("/", createUser);
